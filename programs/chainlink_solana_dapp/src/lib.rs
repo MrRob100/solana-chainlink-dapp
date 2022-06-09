138ce04b6975fe774c132b22eb7a82bdc01be177
Rob A <robarnold100@hotmail.com>
2022-06-09 07:31:02 -0300

    init

---
+++ programs/chainlink_solana_dapp/src/lib.rs
@@ -0,0 +1,36 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use chainlink_solana as chainlink;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod chainlink_solana_dapp {
+    use super::*;
+
+    pub fn execute(ctx: Context<Execute>) -> ProgramResult {
+        let round = chainlink::latest_round_data(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info())?;
+        
+        let result_account = &mut ctx.accounts.result_account;
+        result_account.value = round.answer;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Execute<'info> {
+    #[account(init, payer=user, space=100)]
+    pub result_account: Account<'info, ResultAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>
+    pub system_program: Program<'info> System>,
+    pub chainlink_program: AccountInfo<'info>,
+    pub chainlink_feed: AccountInfo<'info>
+}
+
+#[account]
+pub struct ResultAccount {
+    pub value: i128
+}
